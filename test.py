666ff81aa953f1ebfaf4e71fdf20e762e9c00c42
Andrija <113442840+Andrija-Gorup@users.noreply.github.com>
2023-05-31 20:02:59 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
 import unittest
-import a
+import a_fixed
 import math
 
 class TestClass(unittest.TestCase):
     def test_add(self):
-        manager = a.OperationsManager(1, 0)
+        manager = a_fixed.OperationsManager(1, 0)
         result = manager.perform_division()
+        self.assertTrue(math.isnan(result))
+
+    def test_list_1(self):
+        manager = a_fixed.OperationsManager(1, 10)
+        result = manager.operation_1()
+        self.assertTrue(math.isnan(result))
+
+    def test_list_2(self):
+        manager = a_fixed.OperationsManager(1, -11)
+        result = manager.operation_1()
         self.assertTrue(math.isnan(result))
